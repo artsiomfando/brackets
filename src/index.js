28db9fa1ca233048f27e5ec43bcc50799fd1bae4
Artsiom Fando <37732105+artsiomfando@users.noreply.github.com>
2021-02-08 01:00:15 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let cloneStr = str;
+    let newArr = [];
+
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        newArr.push(bracketsConfig[i][0] + bracketsConfig[i][1]);
+    }
+
+    let k = 1;
+    while (k <= Math.ceil(str.length / 2)) {
+        k++;
+        for (let j = 0; j < newArr.length; j++) {
+            cloneStr = cloneStr.replace(newArr[j], "");
+        }
+    }
+
+    return cloneStr.length === 0 ? true : false;
 }
